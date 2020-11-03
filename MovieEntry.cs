e1c2b2fe2db57a0714de29c5552ff4ee25693a13
Deadspawn11 <64946794+Deadspawn11@users.noreply.github.com>
2020-11-03 18:38:20 -0500

    Update MovieEntry.cs
    Added the code for MovieEntry and changed "Entity" to "Entities".

--- MovieEntry.cs
+++ MovieEntry.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entity
+namespace Entities
 {
     public class MovieEntry
     {
+        public int Theatre { get; set; }
+        public string Title { get; set; }
+        public List<Showtime> Showings { get; set; }
+        public int SeatsAvailable { get; set; }
+        public string PosterPath { get; set; }
+
+        public MovieEntry()
+        {
+            Showings = new List<Showtime>();
+        }
+        public MovieEntry(int theatre, string title, List<Showtime> showings, int seats, string posterpath) 
+        {
+            Theatre = theatre;
+            Title = title;
+            Showings = showings;
+            SeatsAvailable = seats;
+            PosterPath = posterpath;
+        }
+
+        public MovieEntry(int theatre, string title, List<Showtime> showings)
+        {
+            Theatre = theatre;
+            Title = title;
+            Showings = showings;
+        }
+        public void AddTime(Showtime time)
+        {
+            Showings.Add(time);
+        }
     }
 }
