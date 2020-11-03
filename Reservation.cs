4e44562879b3739a4762d1d4178fcbfc3406aa87
Joseph Glass <jdg1125@gmail.com>
2020-11-03 18:22:09 -0500

    added entity objects in the Entity namespace; completed the Showtime class; added using directives to files that depend on the Entity namespace.

--- Reservation.cs
+++ Reservation.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Entity;
 
-namespace Controllers
+namespace Entity
 {
-    public class DBConnector 
+    public class Reservation
     {
-        
     }
 }
---
+++ Showtime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Showtime
+    {
+        public DateTime Day { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set;  }
+
+        public Showtime(DateTime paramDay, DateTime paramStart, DateTime paramEnd)
+        {
+            Day = paramDay;
+            Start = paramStart;
+            End = paramEnd;
+        }
+    }
+}
